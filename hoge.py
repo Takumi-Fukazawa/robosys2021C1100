cba12cea0dbea318611af0178b73e6d1cef799d1
Takumi Fukazawa <s21c1100zh@s.chibakoudai.jp>
2022-10-13 12:36:40 +0900

    Add a command

---
+++ hoge.py
@@ -0,0 +1,11 @@
+mport sys
+
+ s =sys.argv[1]
+
+  if s == "おはよう":
+           print("おやすみ")
+            else s == "おやすみ":
+                     print("おはよう")
+                      else:
+                              print("わからん")
+
