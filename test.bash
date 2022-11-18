d9e40814b362139e96e37e23f88be1ac5d2e9c1e
Takumi Fukazawa <s21c1100zh@s.chibakoudai.jp>
2022-11-18 21:58:45 +0900

    Support integer only calculation

--- test.bash
+++ test.bash
@@ -3,15 +3,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng ( ) {
-        echo NG at Line $1
-	res=1
+       echo NG at Line $1
+       res=1
 }
 res=0
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = "15[1, 3, 5, 15][3, 5]" ] || ng ${LINENO}
 
-### STRANGE IMPUT ###
+out=$( seq 5 | sed 's/$/.1/' | ./plus)
+[ "${out}" = "15.5No divisors.Prime factorization is not possible." ] || ng ${LINENO}
+
+out=$( seq 5 | sed 's/$/.2/' | ./plus)
+[ "${out}" = "16.0[1, 2, 4, 8, 16][2, 2, 2, 2]" ] || ng ${LINENO}
+
+### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
@@ -22,5 +29,13 @@ out=$(echo | ./plus)
 
 [ "$res" = 0 ] && echo OK
   exit $res
+
+
+         
+
+
+
+
+
 
 
